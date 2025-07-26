95394dd56ce3f53bda8125d4782eccd1b5f9f529
bsoltb <java_utils@outlook.com>
2025-07-26 21:38:45 +0800

    upd(feat/i18n)

--- src/i18n/i18n.hpp
+++ src/i18n/i18n.hpp
@@ -7,15 +7,32 @@
 class i18n
 {
     public:
-    inline i18n(std::string lang) {language = lang;};
+    inline i18n(std::string lang) {language = lang;load();};
+    // 获取翻译键值
     std::string of(std::string key);
+    // 数据生成：可以用代码生成翻译键值
     void add(std::string key, std::string val);
+    // 保存生成的数据
     void save();
+    // 获取翻译键值
     void load();
     inline std::string getlang() {return language;};
     private:
     std::map<std::string, std::string> tr;
     std::string language;
 };
+
+// 语言文件地址：i18n:/xx_xx.json e.g.:i18n:/en_us.json（英语-美国）或者i18n:/zh_jbp.json（中文-JBP口音）
 const std::string I18NPATH = "i18n:/";
 const std::string FILESUFFIX = ".json";
+
+i18n EN_US = i18n("en_us");
+i18n ZH_CN = i18n("zh_cn");
+i18n ZH_JBP = i18n("zh_jbp");
+
+// 负责数据生成（语言文件）
+class TranslationDataGen
+{
+    public:
+    void gen();
+};
--- src/server_main.cpp
+++ src/server_main.cpp
@@ -5,27 +5,14 @@
 int main()
 {
     std::cout << "Hello World!" << std::endl;
+    TranslationDataGen().gen();
     std::fstream fst;
     FileManager().getfile("GaMeDaTa:/TesT.tXt",fst,NORMAL);
     std::string hhh;
     fst >> hhh;
     std::cout << hhh << std::endl;
     fst.close();
-    i18n english("en_us");
-    i18n chinese("zh_cn");
-    // chinese.add("nosignal.welcome","你好");
-    // chinese.save();
-    i18n russian("ru_ru");
-    // russian.add("nosignal.welcome","привет");
-    // russian.save();
-    // english.add("nosignal.welcome", "Hello");
-    // english.save();
-    english.load();
-    chinese.load();
-    russian.load();
-    std::cout << english.of("nosignal.welcome") << std::endl;
-    std::cout << chinese.of("nosignal.welcome") << std::endl;
-    std::cout << russian.of("nosignal.welcome") << std::endl;
+
     std::cout << UUID().toString() << std::endl;
     std::cout << UUID().toString2() << std::endl;
     UUID u;
@@ -34,5 +21,7 @@ int main()
         std::cout << static_cast<int>(*i) << " ";
     }
     std::cout << std::endl;
+
+    
     return 0; 
 }
