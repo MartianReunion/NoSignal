f504df75bc0c792c3ad715ccf8dc0907320cb494
bsoltb <java_utils@outlook.com>
2025-07-27 21:48:25 +0800

    upd(feat/filemanager)

--- src/filemanager/filemanager.hpp
+++ src/filemanager/filemanager.hpp
@@ -15,9 +15,9 @@ class FileManager
 {
     public:
     bool getfile(std::string path, std::fstream &s, std::ios::openmode way);
-    bool getfile_utf8(std::string path, std::wfstream &s, std::ios::openmode way);
+    // bool getfile_utf8(std::string path, std::wfstream &s, std::ios::openmode way);
     bool getfile_auto(std::string path, std::fstream &s, std::ios::openmode way); // 新增功能：如果没有就自动创建
-    bool getfile_auto_utf8(std::string path, std::wfstream &s, std::ios::openmode way);
+    // bool getfile_auto_utf8(std::string path, std::wfstream &s, std::ios::openmode way);
     bool existfile(std::string path);
     bool existdir(std::string path);
     // void MakeGameDir();
