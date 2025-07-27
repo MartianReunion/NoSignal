80d88265a9d926005299cc088fe67c18fb5a03d7
bsoltb <java_utils@outlook.com>
2025-07-27 21:42:36 +0800

    upd(i18n)：删除CPP17中弃用的内容

--- src/i18n/utiles.hpp
+++ src/i18n/utiles.hpp
@@ -1,28 +1,28 @@
-#include <codecvt>
-#include <locale>
-#include <string>
+// #include <codecvt>
+// #include <locale>
+// #include <string>
 
-// Not Useful AT ALL! (a stupid AI gives me this)
-std::string gbktoutf8(const std::string& gbkStr) 
-{
-    // Step 1: GBK to Unicode
-    std::wstring_convert<std::codecvt_byname<wchar_t, char, std::mbstate_t>> conv(
-    new std::codecvt_byname<wchar_t, char, std::mbstate_t>("zh_CN.GBK"));
-    std::wstring unicodeStr = conv.from_bytes(gbkStr);
+// // Not Useful AT ALL! (a stupid AI gives me this)
+// std::string gbktoutf8(const std::string& gbkStr) 
+// {
+//     // Step 1: GBK to Unicode
+//     std::wstring_convert<std::codecvt_byname<wchar_t, char, std::mbstate_t>> conv(
+//     new std::codecvt_byname<wchar_t, char, std::mbstate_t>("zh_CN.GBK"));
+//     std::wstring unicodeStr = conv.from_bytes(gbkStr);
 
-    // Step 2: Unicode to UTF-8
-    std::wstring_convert<std::codecvt_utf8<wchar_t>> utf8Conv;
-    return utf8Conv.to_bytes(unicodeStr);
-}
+//     // Step 2: Unicode to UTF-8
+//     std::wstring_convert<std::codecvt_utf8<wchar_t>> utf8Conv;
+//     return utf8Conv.to_bytes(unicodeStr);
+// }
 
-std::string Wstring2String(const std::wstring& wstr)
-{
-    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
-    return converter.to_bytes(wstr);
-}
+// std::string Wstring2String(const std::wstring& wstr)
+// {
+//     std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+//     return converter.to_bytes(wstr);
+// }
 
-std::wstring String2Wstring(const std::string& str)
-{
-    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
-    return converter.from_bytes(str);
-}
+// std::wstring String2Wstring(const std::string& str)
+// {
+//     std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+//     return converter.from_bytes(str);
+// }
